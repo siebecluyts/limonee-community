10f3879b75cd1cc0a31cd9932b094390cd1b903a
SiebeCluyts <debendevanzelem1@gmail.com>
2025-06-28 18:28:01 +0000

    🚀 setup community platform

--- supabase.js
+++ supabase.js
@@ -1,9 +1,7 @@
 // supabase.js
-import { createClient } from '@supabase/supabase-js'
-import dotenv from 'dotenv'
-dotenv.config()
+import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
-)
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
